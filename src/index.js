b4f9ea607cc843d335021236c62a870e589bd4ed
tobias <tobias.datascience@gmail.com>
2021-06-17 02:10:26 +0800

    Add winner function

--- src/index.js
+++ src/index.js
@@ -16,18 +16,27 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
+      turnCount: 0
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    if (calculateWinner(squares) || squares[i]) {
+      // do nothing if winner or square is filled
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O'; // ternary operator;
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+      turnCount: this.state.turnCount + 1
+    });
   }
 
   renderSquare(i) {
     return <Square
-    
       // these are the props passed in
       value={this.state.squares[i]}
       onClick={() => this.handleClick(i)}
@@ -35,11 +44,20 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    const turnCount = this.state.turnCount;
+    
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner + '!';
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
         <div className="status">{status}</div>
+        <div className="turnCount">Turn Count: {turnCount}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -85,3 +103,22 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}

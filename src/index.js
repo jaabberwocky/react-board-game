6d6bafca8d8ea651955d1dd606a987d2f319d742
tobias <tobias.datascience@gmail.com>
2021-06-17 01:54:43 +0800

    Change Square to function componeont

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -27,14 +22,16 @@ class Board extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = 'X';
-    this.setState({squares: squares});
+    this.setState({ squares: squares });
   }
 
   renderSquare(i) {
-    return <Square 
-    value={this.state.squares[i]}
-    onClick={() => this.handleClick(i)}
-     />;
+    return <Square
+    
+      // these are the props passed in
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
